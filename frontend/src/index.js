4bbfe0dc7b31890154ec711c25f967c14a757624
kphan <kphan@hotmail.fr>
2025-01-30 16:43:04 +0100

    V2.1 add react-router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Accommodation from "./pages/Accommodation";
+import Error404 from "./pages/Error404";
+import About from "./pages/About";
 
 
 const rootElement = document.getElementById('root'); // Votre conteneur HTML
@@ -8,6 +12,15 @@ const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
+    <Router>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:id" element={<Accommodation />} />
+          <Route path="*" element={<Error404 />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+
+    </Router>  
+    </React.StrictMode>,
 );
